a82b84602d83be44ab0adbced576eba4a01423b7
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2025-07-15 00:34:45 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -94,3 +94,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running at http://localhost:${PORT}`)
 );
+
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "https://gec-attendance.vercel.app"],
+  })
+);
